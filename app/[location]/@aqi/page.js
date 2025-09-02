bc9e973594d981a98b7ae0773d1facc574fe4eb5
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-09-02 15:47:33 +0600

    feat: load wind, temperature and weather data based on current location

--- app/[location]/@aqi/page.js
+++ app/[location]/@aqi/page.js
@@ -1,5 +1,8 @@
-export default function AirQualityPage() {
-  return (
-    <div>Air Quality Page</div>
-  )
+import AQIComponent from "@/components/AQIComponent";
+
+export default function AirQualityPage({
+	params: { location },
+	searchParams: { latitude, longitude },
+}) {
+	return <AQIComponent lat={latitude} lon={longitude} />;
 }
--- app/[location]/layout.js
+++ app/[location]/layout.js
@@ -15,21 +15,21 @@ export default function LocationLayout({
 }) {
 	return (
 		<div className="wrapper">
+			<div className="overlay"></div>
 			<Image
 				src="/background.png"
 				className="bg-img"
-				height={700}
-				width={1200}
+				width={700}
+				height={1200}
 			/>
-			<div className="overlay"></div>
-			<main class="!z-50 w-full">
-				<div class="container">
-					<div class="grid grid-cols-12 gap-y-8 py-16 lg:gap-8 2xl:gap-20 2xl:py-20">
+			<main className="!z-50 w-full">
+				<div className="container">
+					<div className="grid grid-cols-12 gap-y-8 py-16 lg:gap-8 2xl:gap-20 2xl:py-20">
 						{children}
-						{<div>AQI: {aqi}</div>}
-						{<div>Weather: {weather}</div>}
-						{<div>Temperature: {temperature}</div>}
-						{<div>Wind: {wind}</div>}
+						{weather}
+						{aqi}
+						{wind}
+						{temperature}
 					</div>
 				</div>
 			</main>
